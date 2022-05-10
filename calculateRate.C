76d8b1e7510ef77a487e1ab4f4c555bf92f2c8a6
soohwan lee <62940130+vince502@users.noreply.github.com>
2022-05-11 05:29:41 +0900

    updted

--- calculateRate.C
+++ calculateRate.C
@@ -5,13 +5,16 @@
 #include <future>
 #include <execution>
 
+static int UseNCores = 30;
+
 std::unordered_map<string, std::map<double, double> > result_map;
 std::map<string, TGraph*> graps;
 
 void calculateRate(){
-//	string n_file = "openHLT_Gmenu_JPsiEMB_NoSPClimit_v1230_v8.root";
-	string n_file = "openHLT_Gmenu_JPsi_v1230_v9.root";
+	string n_file = "openHLT_Gmenu_JPsiEMB_NoSPClimit_v1230_v8.root";
+//	string n_file = "openHLT_Gmenu_JPsi_v1230_v9.root";
 
+	//String vector for trigger names
 	std::vector<string> v_names;
 	{
 		RateCalc _calc = RateCalc(n_file.c_str());
@@ -22,14 +25,17 @@ void calculateRate(){
 		}
 	}
 
-	std::vector<double> cuts;
-	int nCores = 15;
-	ROOT::EnableImplicitMT();
-	ROOT::TThreadExecutor mpe(nCores);
+	//Enable MultiThreading
+	ROOT::EnableImplicitMT(UseNCores);
+	ROOT::TThreadExecutor mpe(UseNCores);
 
+	//Define cuts
+	std::vector<double> cuts;
 	for(auto idx : ROOT::TSeqI(11)){
 		cuts.push_back(0.5 *(double) idx);
 	}
+
+	//Run calculator
 	auto extractRates = [=](int idx){
 		RateCalc calc = RateCalc(n_file.c_str());
 		calc.setObjectTree(v_names[idx], cuts);
@@ -37,6 +43,7 @@ void calculateRate(){
 		return calc.getRates();
 	};
 
+	//Map results
 	const auto& res = mpe.Map(extractRates, ROOT::TSeqI(v_names.size()));
 	for( auto idx : res ){
 		for( auto item : idx ){
@@ -44,11 +51,19 @@ void calculateRate(){
 		}
 	}
 
+	//Draw graphs
+	TFile* output = new TFile("output.root", "recreate");
+	output->cd();
 	for( auto result : result_map ){
 		graps[result.first] = new TGraph();
 		for( auto key_val : result.second){
 			graps[result.first]->AddPoint(key_val.first, key_val.second);
 		}
-	};
-
+		graps[result.first]->SetName(result.first.c_str());
+		graps[result.first]->GetXaxis()->SetTitle("p_T cut (GeV/c)");
+		graps[result.first]->GetYaxis()->SetLimits(0,1);
+		graps[result.first]->GetYaxis()->SetRangeUser(0,1);
+		graps[result.first]->Write();
+	}
+	output->Close();
 };
